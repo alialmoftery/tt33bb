cd1e02bd1eaf632ae5490673bf8f81c43b0f748d
MohamedSayan <42085540+MohamedSayan@users.noreply.github.com>
2018-08-19 11:25:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1555,7 +1555,7 @@ client.on('message', message => {
 	var prefix = "#";
 if (message.content.startsWith(prefix + 'tag')) {
     let args = message.content.split(" ").slice(1);
-if(#args[0]) return message.reply('مرجو كتابة نص الدي تريد');  
+if(!args[0]) return message.reply('مرجو كتابة نص الدي تريد');  
 
     figlet(args.join(" "), (err, data) => {
               message.channel.send("```" + data + "```")
